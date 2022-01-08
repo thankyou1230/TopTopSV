a74596ffa0cd6e81b9ebad406f5641b4b559a7e3
Quang Huỳnh Phan Minh <18520140@ms.uit.edu.vn>
2022-01-08 20:30:02 +0700

    mod comment struct

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -10,17 +10,19 @@ namespace TopTopServer.Models
     [Table("Comment")]
     public class Comment
     {
-        public Comment(string user, string video, string CommentContent, DateTime commentTime)
+        public Comment(string user, string video, string CommentContent, DateTime commentTime, string isOwner)
         {
             this.User = user;
             this.Video = video;
             this.CommentContent = CommentContent;
             this.CommentTime = commentTime;
+            this.IsOwner = isOwner;
         }
 
         public string User { get; set; }
         public string Video { get; set; }
         public string CommentContent { get; set; }
         public DateTime CommentTime { get; set; }
+        public string IsOwner { get; set; }
     }
 }
