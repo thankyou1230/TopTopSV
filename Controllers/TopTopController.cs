ae033f37e2818d95383f927f7109fc362d790b39
Quang Huỳnh Phan Minh <18520140@ms.uit.edu.vn>
2022-01-13 11:58:41 +0700

    mod google login feat

--- Controllers/TopTopController.cs
+++ Controllers/TopTopController.cs
@@ -131,6 +131,73 @@ namespace TopTopServer.Controllers
                              GET A USER'S PROFILE BY EMAIL
         ================================================================================*/
         [HttpPost]
+        [Route("FetchGoogleProfile")]
+        public async Task<IActionResult> FetchGoogleProfile()
+        {
+            try
+            {
+                var request = HttpContext.Request;
+                var email = Request.Form["email"];
+                var avatar = Request.Form["avatar"];
+                var nickname = Request.Form["nickname"];
+                //main Profile
+                var profile = await _context.Users.FindAsync(email);
+                if (profile == null)
+                {
+                    var newUser = new User(email, avatar, nickname, "", DateTime.UtcNow.AddHours(7), 0);
+                    _context.Add(newUser);
+                    _context.SaveChanges();
+                }
+                //follower
+                var followersList = await _context.Follows.ToListAsync();
+                var userFollowers = followersList.Where(follow => follow.Following == email);
+                var userFollowersProfiles = userFollowers.Join
+                    (
+                        _context.Users,
+                        follow => follow.Follower,
+                        user => user.Email,
+                        (follow, user) => user
+                    );
+                //following
+                var followingsList = await _context.Follows.ToListAsync();
+                var userFollowings = followingsList.Where(follow => follow.Follower == email);
+                var userFollowingsProfiles = userFollowings.Join
+                    (
+                        _context.Users,
+                        follow => follow.Following,
+                        user => user.Email,
+                        (follow, user) => user
+                    );
+                //like Count
+                var videos = await _context.Videos.ToListAsync();
+                var likeCount = videos.Where(video => video.Owner == email).Sum(i => i.LikeCount);
+                //user's video
+                var allVideos = await _context.Videos.ToListAsync();
+                var videosList = allVideos.Where(video => video.Owner == email);
+                //Liked video
+                var likesList = await _context.Likes.ToListAsync();
+                var likedVideos = likesList.Where(like => like.User == email);
+                var likedVideosWithDetails = likedVideos.Join
+                    (
+                        _context.Videos,
+                        like => like.Video,
+                        video => video.Url,
+                        (like, video) => video
+                    )
+                    .Where(video => video.IsPrivate == 0);
+                var response = JsonConvert.SerializeObject(new { MainProfile = profile, Followers = userFollowersProfiles, Followings = userFollowingsProfiles, LikeCount = likeCount, Videos = videosList, LikedVideos = likedVideosWithDetails });
+                return Ok(response);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        /*==============================================================================
+                             GET A USER'S PROFILE BY EMAIL
+        ================================================================================*/
+        [HttpPost]
         [Route("FollowRequest")]
         public async Task<IActionResult> FollowRequest()
         {
